d67be1267d44959daca01deafe461c0fdcba99e0
xavier-Delfini <xavierdelfini@proton.me>
2025-11-16 23:25:15 +0100

    First Commit, Added a TODO List

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿//TODO: You need to create a game that consists of asking the player what's the result of a math question (i.e. 9 x 9 = ?), collecting the input and adding a point in case of a correct answer.
+//TODO:A game needs to have at least 5 questions.
+//TODO:The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
+//TODO:Users should be presented with a menu to choose an operation
+//TODO:You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games. (No storage after closing the program)
